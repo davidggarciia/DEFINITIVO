81ecc972093e112c1b90c3ca004c47788ee9dcec
davidggarciia <cf22david.gomez@iesjoandaustria.org>
2024-01-29 17:00:16 +0100

    CAMBIOS LUNES

--- app/src/main/java/com/example/definitivo/data/Database.java
+++ app/src/main/java/com/example/definitivo/data/Database.java
@@ -1,19 +1,20 @@
 package com.example.definitivo.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class Database extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "CarritoCompra.db";
+    public static final String DATABASE_NAME = "JaEsticDB";
     public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
 
     }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
 
@@ -115,8 +116,6 @@ public class Database extends SQLiteOpenHelper {
         db.execSQL(insertProduct18);
 
     }
-
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
